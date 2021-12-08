3ce2fd1103f703bdaddb1db5a8181ddbb164c6ba
Gordon Little <glittle@mun.ca>
2021-12-08 15:01:48 -0330

    update

--- test_mean.py
+++ test_mean.py
@@ -1,14 +1,12 @@
 from mean import *
 
 def test_ints():
-	"""test the mean of integers"""
     num_list = [1,2,3,4,5]
     obs = mean(num_list)
     exp = 3
     assert obs == exp
 
 def test_zero():
-	"""Test mean of ingegers including zero"""
     num_list=[0,2,4,6]
     obs = mean(num_list)
     exp = 3
